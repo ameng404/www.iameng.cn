99261dcfb7105f6ed404d4a33a7af9955809a28a
尹猛 <yinmeng.1029@bytedance.com>
2024-04-07 15:08:49 +0800

    feat: 更新网站

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,7 @@
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
-import Heading from "@theme/Heading";
-
-import styles from "./index.module.css";
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero", styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link className="button button--secondary" to="/docs/intro">
-            爱前端
-          </Link>
-          <Link className="button button--secondary" to="/blog">
-            爱生活
-          </Link>
-          <Link className="button button--secondary" to="/docs/intro">
-            爱摄影
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+import HomepageHeader from "./index/Header";
+import HomepageFeatures from "./index/Features";
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
--- src/pages/resources/ResourceCard/index.tsx
+++ src/pages/resources/ResourceCard/index.tsx
@@ -1,19 +1,18 @@
-import React, { memo } from 'react'
-import clsx from 'clsx'
-import Link from '@docusaurus/Link'
+import React, { memo } from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import { type Resource } from "@site/data/resources";
 
-import styles from './styles.module.css'
-import { type Resource } from '@site/data/resources'
-import Tooltip from '@site/src/components/Tooltip'
+import styles from "./styles.module.css";
 
 const ResourceCard = memo(({ resource }: { resource: Resource }) => (
   <li
     key={resource.name}
-    className={clsx(styles.resourceCard, 'padding-vert--sm padding-horiz--md')}
+    className={clsx(styles.resourceCard, "padding-vert--sm padding-horiz--md")}
   >
     <img
       src={
-        typeof resource.logo === 'string'
+        typeof resource.logo === "string"
           ? resource.logo
           : (resource.logo as { src: { src: string } })?.src?.src
       }
@@ -28,16 +27,9 @@ const ResourceCard = memo(({ resource }: { resource: Resource }) => (
           </Link>
         </h4>
       </div>
-      <Tooltip
-        key={resource.name}
-        text={resource.desc}
-        anchorEl="#__docusaurus"
-        id={`tooltip-${resource.name}`}
-      >
-        <p className={styles.resourceCardDesc}>{resource.desc}</p>
-      </Tooltip>
+      <p className={styles.resourceCardDesc}>{resource.desc}</p>
     </div>
   </li>
-))
+));
 
-export default ResourceCard
+export default ResourceCard;
